c8c7c442aca899b39f9628c51bf327b89cf6b87c
chrisjleu <chrisjleu@gmail.com>
2019-04-17 21:23:44 +0200

    Fix path in unit test

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -12,7 +12,7 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/C:/Program Files/Git/hello")
+          .when().get("/hello")
           .then()
              .statusCode(200)
              .body(is("hello"));
